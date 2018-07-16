d69f3884c19e65da5613c4fef8f37dc247dac7e9
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-16 11:57:07 +0700

    test and passed class name valid or not

--- src/main/java/ClassName.java
+++ src/main/java/ClassName.java
@@ -4,9 +4,6 @@ import java.util.regex.Pattern;
 public class ClassName {
     private static final String CLASS_NAME_REGEX = "^[ACP]([A-Za-z0-9]){4}[G-M]$";
 
-    private static final String[] validName = new String[]{" C0318G"};
-    private static final String[] invalidName = new String[]{"M0318G", "P0323A"};
-
     public ClassName() {
 
     }
